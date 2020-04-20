8a91a9fb7297cd1d6e3380daf6b1e712cf4194c9
Tibor Prokai <tibor@kotem.com>
2020-04-20 03:12:11 +0200

    valve controller improved

--- src/heating.rs
+++ src/heating.rs
@@ -24,7 +24,7 @@ where
     mode: Mode,
     state: Roll::<DURATION>,
     drive_time: DURATION,
-    allowed_overload_duration: DURATION,    
+    allowed_overload_duration: DURATION, //during the start only!   
     motor_drive_a: DA,
     motor_drive_b: DB,
     //motor_current_sense: CS,
@@ -48,47 +48,33 @@ where
         }
     }
 
-    pub fn command(&mut self, command: Command<DURATION>)
-    {
-        self.mode = match command {
-            Command::Regular(command) => {      
-                let modified_command = if let Some(max) = self.state.maximum() {
-                    //to avoid overload at the ends, stay away with 1/16 range
-                    match command {
-                        room_pill::roll::Command::Open => room_pill::roll::Command::SetPosition(*max / 16u32),
-                        room_pill::roll::Command::Close => room_pill::roll::Command::SetPosition(*max * 15u32 / 16u32),
-                        _ => command,
-                    }                    
-                } else {
-                    command
-                };
-                self.state.execute(modified_command);
-                Mode::Normal
-            }                
-            Command::Calibrate => Mode::CalibrationPhase1
-        }
-    }
-
-    pub fn update(&mut self, delta_t: DURATION, overload_detected: bool) {
+    pub fn update(&mut self, command: Option<Command<DURATION>>, delta_t: DURATION, power: i32, noise_level: i32, overload_level: i32) {
         if self.state.state() != State::Stopped {
             self.drive_time = self.drive_time + delta_t;
         }
-        //update the valve state machine and apply the state on the HW:            
-        self.state.update(delta_t, !overload_detected || self.drive_time <= self.allowed_overload_duration);        
-        self.apply();
-
-        if self.state.state() == State::Stopped {
-            self.mode = match self.mode {
-                Mode::CalibrationPhase1 => { self.state.execute(room_pill::roll::Command::Open); Mode::CalibrationPhase2 }
-                Mode::CalibrationPhase2 => { self.state.execute(room_pill::roll::Command::Close); Mode::Normal }
-                _ => Mode::Normal
+        if let Some(command) = command {
+			self.mode = match command {
+                Command::Regular(command) => {      
+                    let translated = if let Some(max) = self.state.maximum() {
+                        //to avoid overload at the ends, stay away with 1/16 range
+                        match command {
+                            room_pill::roll::Command::Open => room_pill::roll::Command::SetPosition(*max / 16u32),
+                            room_pill::roll::Command::Close => room_pill::roll::Command::SetPosition(*max * 15u32 / 16u32),
+                            _ => command,
+                        }                    
+                    } else {
+                        command
+                    };
+                    self.state.execute(translated);
+                    Mode::Normal
+                }                
+                Command::Calibrate => Mode::CalibrationPhase1
             }
-        }
-    }
-
-    //renders the state on the hardware
-    fn apply(&mut self)
-    {
+		}        
+        //update the valve state machine and apply the state on the HW:            
+        self.state.update(delta_t, power < overload_level || self.drive_time <= self.allowed_overload_duration);        
+        
+        //render the state on the hardware:
         //a=0 b=1 : closing current: [120 -> 250], block: 300..440
         //a=1 b=0 : opening current: [120..160] avg=143, block: 300..440
         match self.state.state() {
@@ -104,7 +90,13 @@ where
                 self.motor_drive_a.set_low();
                 self.motor_drive_b.set_low();
                 self.drive_time = DURATION::default();
+
+                self.mode = match self.mode {
+                    Mode::CalibrationPhase1 => { self.state.execute(room_pill::roll::Command::Open); Mode::CalibrationPhase2 }
+                    Mode::CalibrationPhase2 => { self.state.execute(room_pill::roll::Command::Close); Mode::Normal }
+                    _ => Mode::Normal
+                }
             }
         }
-    }
+    }    
 }
