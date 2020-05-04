1a0bf1b0379d01ee3f22c5c38415d2bb894ef477
Tibor Prokai <tibor@kotem.com>
2020-05-04 09:55:41 +0200

    debugger connection improved

---
+++ ocd-lila.bat
@@ -0,0 +1,1 @@
+openocd -f "stlink-v2-lila.cfg" -f "stm32f1x.cfg"
--- ocd.bat
+++
@@ -1,7 +0,0 @@
-rem run this first and leave the terminal open: 
-rem itmdump -f itm.fifo -s 0
-openocd -f "stlink-v2.cfg" -f "stm32f1x.cfg"
-
-rem gdb port 5000
-rem telnet port 4444
-rem arm-none-eabi-gdb c:\Eclipse.portable\RustForSTM32\cortex-m-quickstart\target\thumbv7m-none-eabi\debug\libcortex_m_quickstart
